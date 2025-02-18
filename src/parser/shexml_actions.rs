a742d2ee4d46435ebfe6dc1b014f0cdb252c95ea
CANCI0 <martinlol831@gmail.com>
2025-02-18 23:15:06 +0100

    Updated Rustemo generated files

---
+++ src/parser/shexml_actions.rs
@@ -0,0 +1,379 @@
+/// This file is maintained by rustemo but can be modified manually.
+/// All manual changes will be preserved except non-doc comments.
+use rustemo::Token as RustemoToken;
+use super::shexml::{TokenKind, Context};
+pub type Input = str;
+pub type Ctx<'i> = Context<'i, Input>;
+#[allow(dead_code)]
+pub type Token<'i> = RustemoToken<'i, Input, TokenKind>;
+pub type Namespace = String;
+pub fn namespace(_ctx: &Ctx, token: Token) -> Namespace {
+    token.value.into()
+}
+pub type Identifier = String;
+pub fn identifier(_ctx: &Ctx, token: Token) -> Identifier {
+    token.value.into()
+}
+pub type PathLiteral = String;
+pub fn path_literal(_ctx: &Ctx, token: Token) -> PathLiteral {
+    token.value.into()
+}
+pub type Path = String;
+pub fn path(_ctx: &Ctx, token: Token) -> Path {
+    token.value.into()
+}
+pub type ShapePath = String;
+pub fn shape_path(_ctx: &Ctx, token: Token) -> ShapePath {
+    token.value.into()
+}
+pub type Uri = String;
+pub fn uri(_ctx: &Ctx, token: Token) -> Uri {
+    token.value.into()
+}
+#[derive(Debug, Clone)]
+pub struct Shexml {
+    pub declarations: Declaration0,
+    pub shapes: Shape0,
+}
+pub fn shexml_c1(_ctx: &Ctx, declarations: Declaration0, shapes: Shape0) -> Shexml {
+    Shexml { declarations, shapes }
+}
+pub type Declaration1 = Vec<Declaration>;
+pub fn declaration1_c1(
+    _ctx: &Ctx,
+    mut declaration1: Declaration1,
+    declaration: Declaration,
+) -> Declaration1 {
+    declaration1.push(declaration);
+    declaration1
+}
+pub fn declaration1_declaration(_ctx: &Ctx, declaration: Declaration) -> Declaration1 {
+    vec![declaration]
+}
+pub type Declaration0 = Option<Declaration1>;
+pub fn declaration0_declaration1(
+    _ctx: &Ctx,
+    declaration1: Declaration1,
+) -> Declaration0 {
+    Some(declaration1)
+}
+pub fn declaration0_empty(_ctx: &Ctx) -> Declaration0 {
+    None
+}
+pub type Shape1 = Vec<Shape>;
+pub fn shape1_c1(_ctx: &Ctx, mut shape1: Shape1, shape: Shape) -> Shape1 {
+    shape1.push(shape);
+    shape1
+}
+pub fn shape1_shape(_ctx: &Ctx, shape: Shape) -> Shape1 {
+    vec![shape]
+}
+pub type Shape0 = Option<Shape1>;
+pub fn shape0_shape1(_ctx: &Ctx, shape1: Shape1) -> Shape0 {
+    Some(shape1)
+}
+pub fn shape0_empty(_ctx: &Ctx) -> Shape0 {
+    None
+}
+#[derive(Debug, Clone)]
+pub enum Declaration {
+    Prefix(Prefix),
+    Source(Source),
+    Expression(Expression),
+    Iterator(Iterator),
+}
+pub fn declaration_prefix(_ctx: &Ctx, prefix: Prefix) -> Declaration {
+    Declaration::Prefix(prefix)
+}
+pub fn declaration_source(_ctx: &Ctx, source: Source) -> Declaration {
+    Declaration::Source(source)
+}
+pub fn declaration_expression(_ctx: &Ctx, expression: Expression) -> Declaration {
+    Declaration::Expression(expression)
+}
+pub fn declaration_iterator(_ctx: &Ctx, iterator: Iterator) -> Declaration {
+    Declaration::Iterator(iterator)
+}
+#[derive(Debug, Clone)]
+pub struct Prefix {
+    pub identifier: Namespace,
+    pub uri: Uri,
+}
+pub fn prefix_c1(_ctx: &Ctx, identifier: Namespace, uri: Uri) -> Prefix {
+    Prefix { identifier, uri }
+}
+#[derive(Debug, Clone)]
+pub struct Source {
+    pub identifier: Identifier,
+    pub path: Uri,
+}
+pub fn source_c1(_ctx: &Ctx, identifier: Identifier, path: Uri) -> Source {
+    Source { identifier, path }
+}
+#[derive(Debug, Clone)]
+pub struct Expression {
+    pub identifier: Identifier,
+    pub paths: Path1,
+}
+pub fn expression_c1(_ctx: &Ctx, identifier: Identifier, paths: Path1) -> Expression {
+    Expression { identifier, paths }
+}
+pub type Path1 = Vec<Path>;
+pub fn path1_c1(_ctx: &Ctx, mut path1: Path1, path: Path) -> Path1 {
+    path1.push(path);
+    path1
+}
+pub fn path1_path(_ctx: &Ctx, path: Path) -> Path1 {
+    vec![path]
+}
+#[derive(Debug, Clone)]
+pub struct Iterator {
+    pub identifier: Identifier,
+    pub path_type: PathLiteral,
+    pub path: Path,
+    pub fields: Attribute1,
+    pub iterators: Nestedterator0,
+}
+pub fn iterator_c1(
+    _ctx: &Ctx,
+    identifier: Identifier,
+    path_type: PathLiteral,
+    path: Path,
+    fields: Attribute1,
+    iterators: Nestedterator0,
+) -> Iterator {
+    Iterator {
+        identifier,
+        path_type,
+        path,
+        fields,
+        iterators,
+    }
+}
+pub type Attribute1 = Vec<Attribute>;
+pub fn attribute1_c1(
+    _ctx: &Ctx,
+    mut attribute1: Attribute1,
+    attribute: Attribute,
+) -> Attribute1 {
+    attribute1.push(attribute);
+    attribute1
+}
+pub fn attribute1_attribute(_ctx: &Ctx, attribute: Attribute) -> Attribute1 {
+    vec![attribute]
+}
+pub type Nestedterator1 = Vec<Nestedterator>;
+pub fn nestedterator1_c1(
+    _ctx: &Ctx,
+    mut nestedterator1: Nestedterator1,
+    nestedterator: Nestedterator,
+) -> Nestedterator1 {
+    nestedterator1.push(nestedterator);
+    nestedterator1
+}
+pub fn nestedterator1_nestedterator(
+    _ctx: &Ctx,
+    nestedterator: Nestedterator,
+) -> Nestedterator1 {
+    vec![nestedterator]
+}
+pub type Nestedterator0 = Option<Nestedterator1>;
+pub fn nestedterator0_nestedterator1(
+    _ctx: &Ctx,
+    nestedterator1: Nestedterator1,
+) -> Nestedterator0 {
+    Some(nestedterator1)
+}
+pub fn nestedterator0_empty(_ctx: &Ctx) -> Nestedterator0 {
+    None
+}
+#[derive(Debug, Clone)]
+pub struct Nestedterator {
+    pub identifier: Identifier,
+    pub path: Path,
+    pub fields: Attribute1,
+    pub iterators: Iterator0,
+}
+pub fn nestedterator_c1(
+    _ctx: &Ctx,
+    identifier: Identifier,
+    path: Path,
+    fields: Attribute1,
+    iterators: Iterator0,
+) -> Nestedterator {
+    Nestedterator {
+        identifier,
+        path,
+        fields,
+        iterators,
+    }
+}
+pub type Iterator1 = Vec<Box<Iterator>>;
+pub fn iterator1_c1(
+    _ctx: &Ctx,
+    mut iterator1: Iterator1,
+    iterator: Iterator,
+) -> Iterator1 {
+    iterator1.push(Box::new(iterator));
+    iterator1
+}
+pub fn iterator1_iterator(_ctx: &Ctx, iterator: Iterator) -> Iterator1 {
+    vec![Box::new(iterator)]
+}
+pub type Iterator0 = Option<Iterator1>;
+pub fn iterator0_iterator1(_ctx: &Ctx, iterator1: Iterator1) -> Iterator0 {
+    Some(iterator1)
+}
+pub fn iterator0_empty(_ctx: &Ctx) -> Iterator0 {
+    None
+}
+#[derive(Debug, Clone)]
+pub struct Attribute {
+    pub identifier: Identifier,
+    pub path: Path,
+}
+pub fn attribute_c1(_ctx: &Ctx, identifier: Identifier, path: Path) -> Attribute {
+    Attribute { identifier, path }
+}
+#[derive(Debug, Clone)]
+pub struct Shape {
+    pub subject: Subject,
+    pub predicate_objects: PredicateObject0,
+}
+pub fn shape_c1(
+    _ctx: &Ctx,
+    subject: Subject,
+    predicate_objects: PredicateObject0,
+) -> Shape {
+    Shape {
+        subject,
+        predicate_objects,
+    }
+}
+pub type PredicateObject1 = Vec<PredicateObject>;
+pub fn predicate_object1_c1(
+    _ctx: &Ctx,
+    mut predicate_object1: PredicateObject1,
+    predicate_object: PredicateObject,
+) -> PredicateObject1 {
+    predicate_object1.push(predicate_object);
+    predicate_object1
+}
+pub fn predicate_object1_predicate_object(
+    _ctx: &Ctx,
+    predicate_object: PredicateObject,
+) -> PredicateObject1 {
+    vec![predicate_object]
+}
+pub type PredicateObject0 = Option<PredicateObject1>;
+pub fn predicate_object0_predicate_object1(
+    _ctx: &Ctx,
+    predicate_object1: PredicateObject1,
+) -> PredicateObject0 {
+    Some(predicate_object1)
+}
+pub fn predicate_object0_empty(_ctx: &Ctx) -> PredicateObject0 {
+    None
+}
+#[derive(Debug, Clone)]
+pub struct Subject {
+    pub class: Class,
+    pub subject_identifier: SubjectIdentifier,
+}
+pub fn subject_c1(
+    _ctx: &Ctx,
+    class: Class,
+    subject_identifier: SubjectIdentifier,
+) -> Subject {
+    Subject {
+        class,
+        subject_identifier,
+    }
+}
+#[derive(Debug, Clone)]
+pub struct Class {
+    pub namespace: Namespace,
+    pub identifier: Identifier,
+}
+pub fn class_c1(_ctx: &Ctx, namespace: Namespace, identifier: Identifier) -> Class {
+    Class { namespace, identifier }
+}
+#[derive(Debug, Clone)]
+pub struct SubjectIdentifier {
+    pub prefix: NamespaceOpt,
+    pub subject_generator: ShapePath,
+}
+pub fn subject_identifier_c1(
+    _ctx: &Ctx,
+    prefix: NamespaceOpt,
+    subject_generator: ShapePath,
+) -> SubjectIdentifier {
+    SubjectIdentifier {
+        prefix,
+        subject_generator,
+    }
+}
+pub type NamespaceOpt = Option<Namespace>;
+pub fn namespace_opt_namespace(_ctx: &Ctx, namespace: Namespace) -> NamespaceOpt {
+    Some(namespace)
+}
+pub fn namespace_opt_empty(_ctx: &Ctx) -> NamespaceOpt {
+    None
+}
+#[derive(Debug, Clone)]
+pub struct PredicateObject {
+    pub predicate: Predicate,
+    pub object: Object,
+}
+pub fn predicate_object_c1(
+    _ctx: &Ctx,
+    predicate: Predicate,
+    object: Object,
+) -> PredicateObject {
+    PredicateObject {
+        predicate,
+        object,
+    }
+}
+#[derive(Debug, Clone)]
+pub struct Predicate {
+    pub namespace: Namespace,
+    pub identifier: Identifier,
+}
+pub fn predicate_c1(
+    _ctx: &Ctx,
+    namespace: Namespace,
+    identifier: Identifier,
+) -> Predicate {
+    Predicate { namespace, identifier }
+}
+#[derive(Debug, Clone)]
+pub enum Object {
+    DataValue(DataValue),
+    Reference(Reference),
+}
+pub fn object_data_value(_ctx: &Ctx, data_value: DataValue) -> Object {
+    Object::DataValue(data_value)
+}
+pub fn object_reference(_ctx: &Ctx, reference: Reference) -> Object {
+    Object::Reference(reference)
+}
+#[derive(Debug, Clone)]
+pub struct DataValue {
+    pub namespace: NamespaceOpt,
+    pub shape_path: ShapePath,
+}
+pub fn data_value_c1(
+    _ctx: &Ctx,
+    namespace: NamespaceOpt,
+    shape_path: ShapePath,
+) -> DataValue {
+    DataValue { namespace, shape_path }
+}
+#[derive(Debug, Clone)]
+pub struct Reference {
+    pub identifier: Identifier,
+}
+pub fn reference_c1(_ctx: &Ctx, identifier: Identifier) -> Reference {
+    Reference { identifier }
+}
