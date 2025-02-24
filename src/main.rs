28d686bb69ad04207594fe594e7977c8fab42dfa
CANCI0 <martinlol831@gmail.com>
2025-02-24 20:19:45 +0100

    Abstract visitor implemented

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
+use std::fs;
+use std::path::Path;
+use rustemo::Parser;
+use rust_shexml_interpreter::parser::shexml::ShexmlParser;
+use rust_shexml_interpreter::serializer::abstract_visitor::Visitor;
 
 fn main() {
+    let parser = ShexmlParser::new();
+    let input = read_test_file("test.shexml");
+
+    let shexmp = parser.parse(&input).unwrap();
+
+    let mut visitor = ShexmlVisitor {  };
+
+    visitor.visit_shexml(&shexmp);
+}
+
+fn read_test_file(file_name: &str) -> String {
+    let path = Path::new("resources").join(file_name);
+    fs::read_to_string(path).expect(&format!("Failed to read {}", file_name))
+}
+
+struct ShexmlVisitor {}
+impl Visitor<String> for ShexmlVisitor{
 
 }
--- src/serializer/abstract_visitor.rs
+++ src/serializer/abstract_visitor.rs
@@ -1,54 +1,21 @@
 use crate::parser::ast::*;
 
-pub trait Visitor<T> {
-    fn visit_shexml(&mut self, n: &Shexml) -> T;
-    fn visit_declaration(&mut self, n: &Declaration) -> T;
-    fn visit_prefix(&mut self, n: &Prefix) -> T;
-    fn visit_source(&mut self, n: &Source) -> T;
-    fn visit_expression(&mut self, n: &Expression) -> T;
-    fn visit_iterator(&mut self, n: &Iterator) -> T;
-    fn visit_nested_iterator(&mut self, n: &NestedIterator) -> T;
-    fn visit_attribute(&mut self, n: &Attribute) -> T;
-    fn visit_shape(&mut self, n: &Shape) -> T;
-    fn visit_subject(&mut self, n: &Subject) -> T;
-    fn visit_class(&mut self, n: &Class) -> T;
-    fn visit_subject_identifier(&mut self, n: &SubjectIdentifier) -> T;
-    fn visit_predicate_object(&mut self, n: &PredicateObject) -> T;
-    fn visit_predicate(&mut self, n: &Predicate) -> T;
-    fn visit_object(&mut self, n: &Object) -> T;
-    fn visit_data_value(&mut self, n: &DataValue) -> T;
-    fn visit_reference(&mut self, n: &Reference) -> T;
-}
-
-pub struct RmlSerializer {
-    pub output: String,
-}
-
-impl RmlSerializer {
-    pub fn new() -> Self {
-        Self {
-            output: String::new(),
+pub trait Visitor<T> where T: Default{
+    fn visit_shexml(&mut self, n: &Shexml) -> T {
+        if let Some(declarations) = &n.declarations {
+            for decl in declarations {
+                self.visit_declaration(decl);
+            }
         }
-    }
-}
-
-impl Visitor<String> for RmlSerializer {
-    fn visit_shexml(&mut self, n: &Shexml) -> String {
-        let mut result = String::new();
-        let declarations = &n.declarations.clone().unwrap();
-        let shapes = &n.shapes.clone().unwrap();
-
-        for decl in declarations {
-            result.push_str(&self.visit_declaration(decl));
+        if let Some(shapes) = &n.shapes {
+            for shape in shapes {
+                self.visit_shape(shape);
+            }
         }
-
-        for shape in shapes {
-            result.push_str(&self.visit_shape(shape));
-        }
-        result
+        T::default()
     }
 
-    fn visit_declaration(&mut self, n: &Declaration) -> String {
+    fn visit_declaration(&mut self, n: &Declaration) -> T {
         match n {
             Declaration::Prefix(p) => self.visit_prefix(p),
             Declaration::Source(s) => self.visit_source(s),
@@ -57,92 +24,92 @@ impl Visitor<String> for RmlSerializer {
         }
     }
 
-    fn visit_prefix(&mut self, n: &Prefix) -> String {
-        format!("@prefix {}: <{}> .\n", n.identifier, n.uri)
+    fn visit_prefix(&mut self, _: &Prefix) -> T {
+        T::default()
     }
 
-    fn visit_source(&mut self, n: &Source) -> String {
-        format!("<{}> a rml:Source ; rml:source \"{}\" .\n", n.identifier, n.path)
+    fn visit_source(&mut self, _: &Source) -> T {
+        T::default()
     }
 
-    fn visit_expression(&mut self, n: &Expression) -> String {
-        format!("<{}> a rml:ExpressionMap ; rml:referenceFormulation \"{}\" .\n", n.identifier, n.paths.join(","))
+    fn visit_expression(&mut self, _: &Expression) -> T {
+        T::default()
     }
 
-    fn visit_iterator(&mut self, n: &Iterator) -> String {
-        let mut result = format!("<{}> a rml:LogicalSource ; rml:iterator \"{}\" .\n", n.identifier, n.path);
-        let nested_iterators = &n.iterators.clone().unwrap();
-
+    fn visit_iterator(&mut self, n: &Iterator) -> T {
         for field in &n.fields {
-            result.push_str(&self.visit_attribute(field));
+            self.visit_attribute(field);
         }
-        for nested in nested_iterators {
-            result.push_str(&self.visit_nested_iterator(nested));
+        if let Some(nested) = &n.iterators {
+            for nested_iterator in nested {
+                self.visit_nested_iterator(nested_iterator);
+            }
         }
-        result
+        T::default()
     }
 
-    fn visit_nested_iterator(&mut self, n: &NestedIterator) -> String {
-        format!("Nested iterator: {} -> {}\n", n.identifier, n.path)
-    }
-
-    fn visit_attribute(&mut self, n: &Attribute) -> String {
-        format!("Attribute: {} -> {}\n", n.identifier, n.path)
-    }
-
-    fn visit_shape(&mut self, n: &Shape) -> String {
-        let mut i = 1;
-        let mut result = format!(
-            "map:m_{i}                     rr:TriplesMap ;
-                    rml:logicalSource      map:l_260963552 ;
-                    rr:predicateObjectMap  map:po_21 , map:po_20 ;
-                    rr:subjectMap          map:s_5 .", );
-        let predicate_objects = &n.predicate_objects.clone().unwrap();
-
-        for po in predicate_objects {
-            result.push_str(&self.visit_predicate_object(po));
+    fn visit_nested_iterator(&mut self, n: &NestedIterator) -> T {
+        for field in &n.fields {
+            self.visit_attribute(field);
         }
-        result.push_str(".\n");
-        result
-    }
-
-    fn visit_subject(&mut self, n: &Subject) -> String {
-        self.visit_class(&n.class)
-    }
-
-    fn visit_class(&mut self, n: &Class) -> String {
-        format!("{}:{}", n.namespace, n.identifier)
-    }
-
-    fn visit_subject_identifier(&mut self, n: &SubjectIdentifier) -> String {
-        match &n.prefix {
-            Some(prefix) => format!("{}:{}", prefix, n.subject_generator),
-            None => n.subject_generator.clone(),
+        if let Some(iterators) = &n.iterators {
+            for it in iterators {
+                self.visit_iterator(it);
+            }
         }
+        T::default()
     }
 
-    fn visit_predicate_object(&mut self, n: &PredicateObject) -> String {
-        let mut result = format!("  rml:predicateObjectMap [ rml:predicate {}:{} ] ;\n", n.predicate.namespace, n.predicate.identifier);
-        result.push_str(&self.visit_object(&n.object));
-        result
+    fn visit_attribute(&mut self, _: &Attribute) -> T {
+        T::default()
     }
 
-    fn visit_predicate(&mut self, n: &Predicate) -> String {
-        format!("{}:{}", n.namespace, n.identifier)
+    fn visit_shape(&mut self, n: &Shape) -> T {
+        self.visit_subject(&n.subject);
+        if let Some(predicate_objects) = &n.predicate_objects {
+            for po in predicate_objects {
+                self.visit_predicate_object(po);
+            }
+        }
+        T::default()
     }
 
-    fn visit_object(&mut self, n: &Object) -> String {
+    fn visit_subject(&mut self, n: &Subject) -> T {
+        self.visit_class(&n.class);
+        self.visit_subject_identifier(&n.subject_identifier);
+        T::default()
+    }
+
+    fn visit_class(&mut self, _: &Class) -> T {
+        T::default()
+    }
+
+    fn visit_subject_identifier(&mut self, _: &SubjectIdentifier) -> T {
+        T::default()
+    }
+
+    fn visit_predicate_object(&mut self, n: &PredicateObject) -> T {
+        self.visit_predicate(&n.predicate);
+        self.visit_object(&n.object);
+        T::default()
+    }
+
+    fn visit_predicate(&mut self, _: &Predicate) -> T {
+        T::default()
+    }
+
+    fn visit_object(&mut self, n: &Object) -> T {
         match n {
             Object::DataValue(dv) => self.visit_data_value(dv),
             Object::Reference(r) => self.visit_reference(r),
         }
     }
 
-    fn visit_data_value(&mut self, n: &DataValue) -> String {
-        format!("  rml:objectMap [ rml:template \"{}\" ] ;\n", n.shape_path)
+    fn visit_data_value(&mut self, _: &DataValue) -> T {
+        T::default()
     }
 
-    fn visit_reference(&mut self, n: &Reference) -> String {
-        format!("  rml:objectMap [ rml:parentTriplesMap \"{}\" ] ;\n", n.identifier)
+    fn visit_reference(&mut self, _: &Reference) -> T {
+        T::default()
     }
 }
